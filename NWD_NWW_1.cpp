61345ebbbdc189764423a5258d19d8e94fe1c3db
Martyna Midzio <xmartimintx@gmail.com>
2022-04-01 10:06:58 +0200

    xD

---
+++ NWD_NWW_1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int a, b, iloczyn, licznik;
+
+    cout << "Podaj pierwszą liczbę: " << endl;
+    cin >> a;
+
+    cout << "Podaj drugą liczbę: " << endl;
+    cin >> b;
+
+    iloczyn = a * b;
+    licznik = 0;
+
+    while (a != b) {
+        if (a < b) {
+            b = b - a;
+
+        }
+
+        else {
+
+            a = a - b;
+        }
+
+
+
+        licznik = licznik + 1;
+
+
+    }
+
+
+
+    cout << "Największy wspólny dzielnik: " << a << endl;
+
+    cout << "Najmniejsza wspólna wielokrotność: " << iloczyn / a << endl;
+
+    cout << "Pętla wykonała się: " << licznik << " razy" << endl;
+
+
+
+
+
+}
