6a60451680c735dcd457d7f48aca471ddcccad45
Martyna Midzio <xmartimintx@gmail.com>
2022-04-20 10:46:20 +0200

    Updated

---
+++ ciąg_Fibonacciego.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, f0, f1, f;
+	cout << "Podaj ile wyrazów ciągu wypisać: ";
+	cin >> n;
+
+	f0 = 0;
+	f1 = 1;
+
+	for (int i = 0; i < n+1; i = i + 1) {
+
+		if (i > 1) {
+
+			f = f0 + f1;
+			f0 = f1;
+			f1 = f;
+		}
+		else {
+
+			f = i;
+			
+		}
+
+		cout << f <<endl;
+	}
+
+
+
+
+
+}
