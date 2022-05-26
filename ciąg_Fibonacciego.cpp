339db7cc35123415ba2d786e2e7edd947b8b782a
xmartimintx <90179460+xmartimintx@users.noreply.github.com>
2022-05-26 09:34:50 +0200

    Update ciąg_Fibonacciego.cpp

--- ciąg_Fibonacciego.cpp
+++ ciąg_Fibonacciego.cpp
@@ -1,17 +1,20 @@
-﻿#include <iostream>
-
-using namespace std;
+#include <iostream>
+#include <chrono>
+#include <ctime>
+using namespace std::chrono;
 
 int main()
 {
 	int n, f0, f1, f;
-	cout << "Podaj ile wyrazów ciągu wypisać: ";
-	cin >> n;
+	std::cout << "Podaj ile wyrazów ciągu wypisać: ";
+	std::cin >> n;
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+	start = std::chrono::system_clock::now();
 
 	f0 = 0;
 	f1 = 1;
 
-	for (int i = 0; i < n+1; i = i + 1) {
+	for (int i = 0; i < n + 1; i = i + 1) {
 
 		if (i > 1) {
 
@@ -22,11 +25,18 @@ int main()
 		else {
 
 			f = i;
-			
+
 		}
 
-		cout << f <<endl;
+		std::cout << f << std::endl;
 	}
+
+	end = std::chrono::system_clock::now();
+
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+	std::cout << "Kod wykonal sie w : " << elapsed_seconds.count() << "sekund" << std::endl;
 
 
 
