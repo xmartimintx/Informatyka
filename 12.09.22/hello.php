c544ad70581e17f241830ef411be90ba027cd0d1
Martyna <xmartimintx@gmail.com>
2022-09-12 12:51:10 +0200

    xD

---
+++ 12.09.22/hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Hello </title>
+</head>
+<body>
+    <?php 
+    echo "Hello World";
+    echo "<br>";
+    $zmienna1=1;
+    $zmienna2="My counter= ";
+    $zmienna3=55;
+    echo $zmienna2.$zmienna1;
+    echo "<br>";
+    echo "My counter v2 ".$zmienna3;
+    ?>
+</body>
+</html>
